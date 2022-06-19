24591ffdfecb5b81c26fdf9dc4fa46e5c4580921
briankrou <bguevaravan@gmail.com>
2022-06-19 13:15:11 -0500

    secon commit

--- src/juegomemoriamultiplicacion/Principal.java
+++ src/juegomemoriamultiplicacion/Principal.java
@@ -20,6 +20,7 @@ public class Principal extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("hola");
         launch(args);
     }
 
